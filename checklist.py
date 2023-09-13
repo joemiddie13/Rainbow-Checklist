4340ddd6bd15ea4ecbbb25550cb223014ea409fa
joemiddie13 <joemiddie13@gmail.com>
2023-09-13 15:17:46 -0700

    Initial commit

---
+++ checklist.py
@@ -0,0 +1,16 @@
+# checklist = list()
+# print(checklist)
+
+checklist = []
+
+# say_this is a parameter or input
+def my_fun_function(say_this):
+    print(say_this)
+
+my_fun_function("Hello World")
+
+checklist = list()
+checklist.append('Blue')
+print(checklist)
+checklist.append('Orange')
+print(checklist)
