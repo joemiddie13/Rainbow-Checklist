1a34a31856801dad3f17600ca0ed28c0ac583240
joemiddie13 <joemiddie13@gmail.com>
2023-09-16 21:38:52 -0700

    Implemented the main program while loop.

--- checklist.py
+++ checklist.py
@@ -1,109 +1,7 @@
-# # checklist = list()
-# # print(checklist)
-
-# checklist = []
-
-# # say_this is a parameter or input
-# def my_fun_function(say_this):
-#     print(say_this)
-
-# my_fun_function("Hello World")
-
-# checklist = list()
-# checklist.append('Blue')
-# print(checklist)
-# checklist.append('Orange')
-# print(checklist)
-
-# #CREATE
-# def create(item):
-#     checklist.append(item)
-
-# checklist = ['Blue', 'Orange']
-# checklist[1] = 'Cats'
-# print(checklist)
-
-# # READ
-# def read(index):
-#     return checklist[index]
-
-# # UPDATE
-# def update(index, item):
-#     checklist[index] = item
-
-# checklist = ['Blue', 'Cats']
-# checklist.pop(1)
-# print(checklist)
-
-################################################################
-
-# # DESTROY
-# def destroy(index):
-#     checklist.pop(index)
-
-# # Initialize an empty list called checklist
-# checklist = []
-
-# # CREATE
-# def create(item):
-#     checklist.append(item)
-
-# # READ
-# def read(index):
-#     return checklist[index]
-
-# # UPDATE
-# def update(index, item):
-#     checklist[index] = item
-
-# # DESTROY
-# def destroy(index):
-#     checklist.pop(index)
-
-# # Test function to validate the CRUD operations
-# def test():
-#     # Test Create
-#     create("purple sox")
-#     create("red cloak")
-#     print("After create:", checklist)
-    
-#     # Test Read
-#     print("Read index 0:", read(0))
-#     print("Read index 1:", read(1))
-    
-#     # Test Update
-#     update(0, "purple socks")
-#     print("After update:", checklist)
-    
-#     # Test Destroy
-#     destroy(1)
-#     print("After destroy:", checklist)
-
-# # Test the function to validate CRUD process
-# test()
-
-
-# list_all_items()
-
-# def test():
-#     create("purple sox")
-#     create("red cloak")
-
-#     print(read(0))
-#     print(read(1))
-
-#     update(0, "purple socks")
-
-#     destroy(1)
-
-#     print(read(0))
-
-#     list_all_items()
-
-################################################################
-
 # Initialize an empty list called checklist
 checklist = []
+
+# List of functions:
 
 # CREATE
 def create(item):
@@ -130,9 +28,27 @@ def list_all_items():
         index += 1
 
 # mark_completed
-
 def mark_completed(index):
     checklist[index] = f"√ {checklist[index]}"
+
+# User input function
+def user_input(prompt):
+    return input(prompt)
+
+# Function to receive input from user
+def select(function_code):
+    if function_code == "C":
+        create (input("Enter the item: "))
+    elif function_code == "R":
+        index = int(input("Which index to read? "))
+        print(read(index))
+    elif function_code == "P":
+        list_all_items()
+    elif function_code == "Q":
+        return False
+    else:
+        print("Unknown option was chosen.")
+    return True
 
 # Test function to validate the CRUD operations
 def test():
@@ -162,3 +78,9 @@ def test():
 
 # Test the function to validate CRUD process
 test()
+
+# Entire program loop
+running = True
+while running:
+    selection = user_input("Press C to add to list, R to read from list, P to display the list, and Q to quit: ")
+    running = select(selection)
