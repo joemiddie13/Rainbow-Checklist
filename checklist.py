390718b20844d52cc157d327230d50e76e6c2eb5
joemiddie13 <joemiddie13@gmail.com>
2023-09-16 21:13:05 -0700

    Created helper functions

--- checklist.py
+++ checklist.py
@@ -1,43 +1,106 @@
+# # checklist = list()
+# # print(checklist)
+
+# checklist = []
+
+# # say_this is a parameter or input
+# def my_fun_function(say_this):
+#     print(say_this)
+
+# my_fun_function("Hello World")
+
 # checklist = list()
+# checklist.append('Blue')
+# print(checklist)
+# checklist.append('Orange')
 # print(checklist)
 
-checklist = []
+# #CREATE
+# def create(item):
+#     checklist.append(item)
 
-# say_this is a parameter or input
-def my_fun_function(say_this):
-    print(say_this)
+# checklist = ['Blue', 'Orange']
+# checklist[1] = 'Cats'
+# print(checklist)
 
-my_fun_function("Hello World")
+# # READ
+# def read(index):
+#     return checklist[index]
 
-checklist = list()
-checklist.append('Blue')
-print(checklist)
-checklist.append('Orange')
-print(checklist)
+# # UPDATE
+# def update(index, item):
+#     checklist[index] = item
 
-#CREATE
-def create(item):
-    checklist.append(item)
+# checklist = ['Blue', 'Cats']
+# checklist.pop(1)
+# print(checklist)
 
-checklist = ['Blue', 'Orange']
-checklist[1] = 'Cats'
-print(checklist)
+################################################################
 
-# READ
-def read(index):
-    return checklist[index]
+# # DESTROY
+# def destroy(index):
+#     checklist.pop(index)
 
-# UPDATE
-def update(index, item):
-    checklist[index] = item
+# # Initialize an empty list called checklist
+# checklist = []
 
-checklist = ['Blue', 'Cats']
-checklist.pop(1)
-print(checklist)
+# # CREATE
+# def create(item):
+#     checklist.append(item)
 
-# DESTROY
-def destroy(index):
-    checklist.pop(index)
+# # READ
+# def read(index):
+#     return checklist[index]
+
+# # UPDATE
+# def update(index, item):
+#     checklist[index] = item
+
+# # DESTROY
+# def destroy(index):
+#     checklist.pop(index)
+
+# # Test function to validate the CRUD operations
+# def test():
+#     # Test Create
+#     create("purple sox")
+#     create("red cloak")
+#     print("After create:", checklist)
+    
+#     # Test Read
+#     print("Read index 0:", read(0))
+#     print("Read index 1:", read(1))
+    
+#     # Test Update
+#     update(0, "purple socks")
+#     print("After update:", checklist)
+    
+#     # Test Destroy
+#     destroy(1)
+#     print("After destroy:", checklist)
+
+# # Test the function to validate CRUD process
+# test()
+
+
+# list_all_items()
+
+# def test():
+#     create("purple sox")
+#     create("red cloak")
+
+#     print(read(0))
+#     print(read(1))
+
+#     update(0, "purple socks")
+
+#     destroy(1)
+
+#     print(read(0))
+
+#     list_all_items()
+
+################################################################
 
 # Initialize an empty list called checklist
 checklist = []
@@ -58,12 +121,25 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+# List all items along with their index
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(f"{index} {list_item}")
+        index += 1
+
+# mark_completed
+
+def mark_completed(index):
+    checklist[index] = f"√ {checklist[index]}"
+
 # Test function to validate the CRUD operations
 def test():
     # Test Create
     create("purple sox")
     create("red cloak")
-    print("After create:", checklist)
+    # print("After create:", checklist)
     
     # Test Read
     print("Read index 0:", read(0))
@@ -76,6 +152,13 @@ def test():
     # Test Destroy
     destroy(1)
     print("After destroy:", checklist)
+
+    # Test List All Items
+    list_all_items()
+
+    # Test mark as completed
+    mark_completed(0)
+    list_all_items()
 
 # Test the function to validate CRUD process
 test()
